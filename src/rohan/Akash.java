1f8bc719d148664a5b87c2f9325548d7ac7b3ed5
unknown <shindesky11@gmail.com>
2022-11-14 17:50:33 +0530

    My third commit

--- src/rohan/Akash.java
+++ src/rohan/Akash.java
@@ -1,6 +1,10 @@
 package rohan;
 
 public class Akash {
+	public static void main(String[] args) {
+		Akash a = new Akash();
+		a.syso();
+	}
 	private void syso() {
 		System.out.println("hello");
 
